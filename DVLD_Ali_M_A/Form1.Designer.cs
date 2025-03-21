e30f383a7f7ebe40222fb73610eac7c0a22caf39
AliMortadaAnani <alimortadaanani@gmail.com>
2025-03-21 02:52:56 +0200

    Add project files.

---
+++ DVLD_Ali_M_A/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace DVLD_Ali_M_A
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            kryptonLabel1 = new Krypton.Toolkit.KryptonLabel();
+            kryptonPanel1 = new Krypton.Toolkit.KryptonPanel();
+            kryptonPanel2 = new Krypton.Toolkit.KryptonPanel();
+            kryptonTextBox1 = new Krypton.Toolkit.KryptonTextBox();
+            kryptonButton2 = new Krypton.Toolkit.KryptonButton();
+            kryptonButton1 = new Krypton.Toolkit.KryptonButton();
+            kryptonPictureBox2 = new Krypton.Toolkit.KryptonPictureBox();
+            kryptonPictureBox1 = new Krypton.Toolkit.KryptonPictureBox();
+            kryptonLabel2 = new Krypton.Toolkit.KryptonLabel();
+            ((System.ComponentModel.ISupportInitialize)kryptonPanel1).BeginInit();
+            kryptonPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)kryptonPanel2).BeginInit();
+            kryptonPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)kryptonPictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonPictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // kryptonLabel1
+            // 
+            kryptonLabel1.AutoSize = false;
+            kryptonLabel1.LabelStyle = Krypton.Toolkit.LabelStyle.TitlePanel;
+            kryptonLabel1.Location = new Point(67, 55);
+            kryptonLabel1.Name = "kryptonLabel1";
+            kryptonLabel1.PaletteMode = Krypton.Toolkit.PaletteMode.VisualStudio2010Render365;
+            kryptonLabel1.Size = new Size(550, 205);
+            kryptonLabel1.StateNormal.ShortText.Color1 = Color.LightGray;
+            kryptonLabel1.StateNormal.ShortText.Font = new Font("Segoe UI Black", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            kryptonLabel1.TabIndex = 0;
+            kryptonLabel1.Values.Text = "Drivers && Vehicules\r\nLicenses Department\r\nSystem\r\n";
+            // 
+            // kryptonPanel1
+            // 
+            kryptonPanel1.Controls.Add(kryptonTextBox1);
+            kryptonPanel1.Controls.Add(kryptonPanel2);
+            kryptonPanel1.Controls.Add(kryptonLabel1);
+            kryptonPanel1.Dock = DockStyle.Fill;
+            kryptonPanel1.Location = new Point(0, 0);
+            kryptonPanel1.Name = "kryptonPanel1";
+            kryptonPanel1.Size = new Size(1280, 720);
+            kryptonPanel1.StateNormal.Color1 = Color.FromArgb(64, 0, 0);
+            kryptonPanel1.StateNormal.Color2 = Color.Black;
+            kryptonPanel1.StateNormal.ColorStyle = Krypton.Toolkit.PaletteColorStyle.Rounded;
+            kryptonPanel1.TabIndex = 1;
+            // 
+            // kryptonPanel2
+            // 
+            kryptonPanel2.Controls.Add(kryptonButton2);
+            kryptonPanel2.Controls.Add(kryptonButton1);
+            kryptonPanel2.Controls.Add(kryptonPictureBox2);
+            kryptonPanel2.Controls.Add(kryptonPictureBox1);
+            kryptonPanel2.Controls.Add(kryptonLabel2);
+            kryptonPanel2.Location = new Point(652, 55);
+            kryptonPanel2.Name = "kryptonPanel2";
+            kryptonPanel2.Size = new Size(564, 602);
+            kryptonPanel2.StateNormal.Color1 = Color.FromArgb(64, 0, 0);
+            kryptonPanel2.StateNormal.Color2 = Color.Black;
+            kryptonPanel2.StateNormal.ColorStyle = Krypton.Toolkit.PaletteColorStyle.GlassCheckedTrackingStump;
+            kryptonPanel2.TabIndex = 3;
+            kryptonPanel2.Paint += kryptonPanel2_Paint;
+            // 
+            // kryptonTextBox1
+            // 
+            kryptonTextBox1.AlwaysActive = false;
+            kryptonTextBox1.Location = new Point(196, 450);
+            kryptonTextBox1.Name = "kryptonTextBox1";
+            kryptonTextBox1.Size = new Size(242, 32);
+            kryptonTextBox1.StateActive.Border.Color1 = Color.Transparent;
+            kryptonTextBox1.StateDisabled.Content.Color1 = Color.Transparent;
+            kryptonTextBox1.StateNormal.Back.Color1 = Color.Transparent;
+            kryptonTextBox1.TabIndex = 4;
+            kryptonTextBox1.Text = "Username";
+            // 
+            // kryptonButton2
+            // 
+            kryptonButton2.Location = new Point(46, 440);
+            kryptonButton2.Name = "kryptonButton2";
+            kryptonButton2.Size = new Size(500, 1);
+            kryptonButton2.TabIndex = 5;
+            kryptonButton2.Values.DropDownArrowColor = Color.Empty;
+            kryptonButton2.Values.Text = "";
+            // 
+            // kryptonButton1
+            // 
+            kryptonButton1.Location = new Point(46, 266);
+            kryptonButton1.Name = "kryptonButton1";
+            kryptonButton1.Size = new Size(500, 1);
+            kryptonButton1.TabIndex = 4;
+            kryptonButton1.Values.DropDownArrowColor = Color.Empty;
+            kryptonButton1.Values.Text = "";
+            // 
+            // kryptonPictureBox2
+            // 
+            kryptonPictureBox2.Image = (Image)resources.GetObject("kryptonPictureBox2.Image");
+            kryptonPictureBox2.Location = new Point(32, 341);
+            kryptonPictureBox2.Name = "kryptonPictureBox2";
+            kryptonPictureBox2.Size = new Size(107, 55);
+            kryptonPictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            kryptonPictureBox2.TabIndex = 5;
+            kryptonPictureBox2.TabStop = false;
+            // 
+            // kryptonPictureBox1
+            // 
+            kryptonPictureBox1.Image = (Image)resources.GetObject("kryptonPictureBox1.Image");
+            kryptonPictureBox1.Location = new Point(32, 181);
+            kryptonPictureBox1.Name = "kryptonPictureBox1";
+            kryptonPictureBox1.Size = new Size(107, 55);
+            kryptonPictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            kryptonPictureBox1.TabIndex = 4;
+            kryptonPictureBox1.TabStop = false;
+            // 
+            // kryptonLabel2
+            // 
+            kryptonLabel2.Location = new Point(202, 30);
+            kryptonLabel2.Name = "kryptonLabel2";
+            kryptonLabel2.Size = new Size(153, 56);
+            kryptonLabel2.StateNormal.ShortText.Color1 = Color.FromArgb(64, 0, 0);
+            kryptonLabel2.StateNormal.ShortText.Color2 = Color.Black;
+            kryptonLabel2.StateNormal.ShortText.ColorStyle = Krypton.Toolkit.PaletteColorStyle.LinearShadow;
+            kryptonLabel2.StateNormal.ShortText.Font = new Font("Tahoma", 20F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            kryptonLabel2.TabIndex = 0;
+            kryptonLabel2.Values.Text = "Log In";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 64);
+            ClientSize = new Size(1280, 720);
+            Controls.Add(kryptonPanel1);
+            ForeColor = SystemColors.ControlText;
+            FormBorderStyle = FormBorderStyle.None;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)kryptonPanel1).EndInit();
+            kryptonPanel1.ResumeLayout(false);
+            kryptonPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)kryptonPanel2).EndInit();
+            kryptonPanel2.ResumeLayout(false);
+            kryptonPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)kryptonPictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonPictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private Krypton.Toolkit.KryptonPanel kryptonPanel1;
+        private Krypton.Toolkit.KryptonPanel kryptonPanel2;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel2;
+        private Krypton.Toolkit.KryptonPictureBox kryptonPictureBox1;
+        private Krypton.Toolkit.KryptonPictureBox kryptonPictureBox2;
+        private Krypton.Toolkit.KryptonButton kryptonButton1;
+        private Krypton.Toolkit.KryptonButton kryptonButton2;
+        private Krypton.Toolkit.KryptonTextBox kryptonTextBox1;
+    }
+}
---
+++ DVLD_Ali_M_A/Form1.cs
@@ -0,0 +1,15 @@
+namespace DVLD_Ali_M_A
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void kryptonPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
